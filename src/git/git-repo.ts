3d6368305ce8236fec45f8608e5a35ce9e1dfa77
Timo Kössler <info@timokoessler.de>
2024-04-06 13:15:26 +0200

    Parse fanout table + binary search

--- src/git/git-repo.ts
+++ src/git/git-repo.ts
@@ -124,23 +124,66 @@ export default class GitRepo {
 
     /**
      * Read packfile index
+     * https://git-scm.com/docs/pack-format
+     * @param packSha The sha1 hash of the packfile
+     * @param sha The sha1 hash of the object to find
      */
-    async readPackIndex(sha: string) {
-        const indexFile = `${this.path}/objects/pack/pack-${sha}.idx`;
-        if (await checkFileExists(indexFile)) {
-            const buf = await readFile(indexFile);
-            const header = buf.subarray(0, 4).toString('hex');
-            // Magic number for pack index file \377tOc
-            if (header !== 'ff744f63') {
-                throw new Error('Invalid pack index file');
-            }
-            const version = buf.readUInt32BE(4);
-            if (version !== 2) {
-                throw new Error('Unsupported pack index version. Only version 2 is supported.');
-            }
-            // Todo
+    async getObjectFromPack(packSha: string, sha: string) {
+        const indexFile = `${this.path}/objects/pack/pack-${packSha}.idx`;
+        if (!(await checkFileExists(indexFile))) {
+            throw new Error(`Pack index file ${indexFile} not found`);
         }
-        throw new Error(`Pack index file ${indexFile} not found`);
+        const buf = await readFile(indexFile);
+        const header = buf.subarray(0, 4).toString('hex');
+        // Magic number for pack index file \377tOc
+        if (header !== 'ff744f63') {
+            throw new Error('Invalid pack index file');
+        }
+        const version = buf.readUInt32BE(4);
+        if (version !== 2) {
+            throw new Error('Unsupported pack index version. Only version 2 is supported.');
+        }
+        let offset = 8; // 4 bytes for header, 4 bytes for version
+
+        // Read fan-out table
+        // The fan-out table is a table of 256 4-byte entries, one for each possible byte value
+        // The n-th entry is the number of objects in the packfile that start with a byte value of n or less
+        const fanoutTable: number[] = [];
+        for (let i = 0; i < 256; i++) {
+            const count = buf.readUInt32BE(offset);
+            fanoutTable.push(count);
+            offset += 4;
+        }
+
+        const prefixNumber = parseInt(sha.substring(0, 2), 16);
+        const sha1Length = 20; // bytes
+
+        // Determine the start position in the packfile based on the fan-out table
+        let startOffset = offset + (prefixNumber > 0 ? fanoutTable[prefixNumber - 1] : 0) * sha1Length;
+        //let endOffset = offset + fanoutTable[prefixNumber] * sha1Length;
+        // Todo test
+        let endOffset = offset + (fanoutTable[prefixNumber] + 6) * sha1Length;
+
+        // Binary search the pack index to find the sha
+        while (startOffset < endOffset) {
+            const mid = startOffset + Math.floor((endOffset - startOffset) / sha1Length / 2) * sha1Length;
+            const midSha = buf.subarray(mid, mid + sha1Length).toString('hex');
+            if (midSha < sha) {
+                startOffset = mid + sha1Length;
+            } else if (midSha > sha) {
+                endOffset = mid;
+            } else {
+                // Found the sha
+                break;
+            }
+        }
+        if (startOffset >= endOffset) {
+            return null;
+        }
+        console.log(startOffset, endOffset);
+        console.log(buf.subarray(startOffset, endOffset).toString('hex'));
+
+        // 67577c0763d0129c9be3d6dc403119656bf2efb5
     }
 
     /**
